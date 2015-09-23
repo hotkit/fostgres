0dea548b0ce2de6247f8ea8c6d43b649e8bfab9f
Kirit Sælensminde <k@kirit.com>
2015-09-23 08:32:55 +0700

    Empty skeleton for fostgres.

--- fostgres/Cpp/fostgres/fostgres.cpp
+++ fostgres/Cpp/fostgres/fostgres.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2009, Felspar Co Ltd. http://fost.3.felspar.com/
+    Copyright 2009-2015, Felspar Co Ltd. http://support.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -7,13 +7,11 @@
 
 
 #include <fost/main>
-#include <hello/hello-lib>
 
 
 FSL_MAIN(
-    "hello-world",
-    "Hello world program example"
+    "fostgres",
+    "fostgres web server"
 )( fostlib::ostream &out, fostlib::arguments &args ) {
-    out << hello_world::text() << std::endl;
     return 0;
 }
