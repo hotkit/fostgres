cf08a9482d1acefe54194419a633ad09f52a9b72
Kirit Sælensminde <k@kirit.com>
2016-08-26 19:37:03 +0700

    Skeleton for CSJ library.

---
+++ Cpp/include/fost/csj.iterator.hpp
@@ -0,0 +1,30 @@
+/*
+    Copyright 2016, Felspar Co Ltd. http://support.felspar.com/
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+*/
+
+
+#pragma once
+
+
+#include <fost/core>
+
+
+namespace fostlib {
+
+
+    namespace csj {
+
+
+        /// Iterate over a CSJ source to produce JSON objects
+        class const_iterator {
+        };
+
+
+    }
+
+
+}
+
