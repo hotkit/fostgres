267d42962e522a4e696136a7796827109de23427
Kirit Sælensminde <k@kirit.com>
2016-08-27 17:52:18 +0700

    Refactor the parser to break out the sexpr bit.

--- Cpp/fostgres-test/fg.parser.cpp
+++ Cpp/fostgres-test/fg.parser.cpp
@@ -12,6 +12,42 @@
 
 
 namespace {
+    const auto space_p = boost::spirit::chlit<wchar_t>(L' ');
+    const auto newline_p = boost::spirit::chlit<wchar_t>(L'\n');
+    const auto string_p = +(boost::spirit::anychar_p - space_p - newline_p);
+    const auto comment_p = *space_p >> boost::spirit::chlit<wchar_t>(L'#')
+        >> *(boost::spirit::anychar_p - newline_p);
+    const fostlib::json_string_parser json_string_p;
+    const fostlib::json_embedded_parser json_ep;
+    const fostlib::json_parser json_p;
+
+    struct sexpr_closure : boost::spirit::closure<sexpr_closure, fg::json> {
+        member1 value;
+    };
+
+    const struct sexpr_parser : public boost::spirit::grammar<
+        sexpr_parser, sexpr_closure::context_t
+    > {
+        sexpr_parser() {}
+
+        template< typename scanner_t >
+        struct definition {
+            definition(sexpr_parser const& self) {
+                top =
+                        expr[fostlib::parsers::push_back(self.value, phoenix::arg1)]
+                        >> *(*space_p
+                            >> expr[fostlib::parsers::push_back(self.value, phoenix::arg1)]);
+
+                expr =
+                    json_ep[expr.value = phoenix::arg1]
+                    | string_p[expr.value = phoenix::construct_<fostlib::string>(phoenix::arg1, phoenix::arg2)];
+            }
+            boost::spirit::rule<scanner_t, sexpr_closure::context_t> expr;
+            boost::spirit::rule<scanner_t> top;
+
+            boost::spirit::rule<scanner_t> const &start() const { return top; }
+        };
+    } sexpr_p;
 }
 
 
@@ -19,38 +55,13 @@ fostlib::json fg::parse(const boost::filesystem::path &filename) {
     fostlib::string code(fostlib::utf::load_file(filename));
     fostlib::json script;
     fostlib::push_back(script, "progn");
-    std::size_t line = 1u;
-
-    auto space_p = boost::spirit::chlit<wchar_t>(L' ');
-    auto newline_p = boost::spirit::chlit<wchar_t>(L'\n');
-    auto string_p = +(boost::spirit::anychar_p - space_p - newline_p);
-    auto comment_p = *space_p >> boost::spirit::chlit<wchar_t>(L'#')
-        >> *(boost::spirit::anychar_p - newline_p);
-    fostlib::json_string_parser json_string_p;
-    fostlib::json_parser json_p;
 
     auto result = boost::spirit::parse(code.c_str(), *(
         newline_p
         | comment_p
-        | (string_p
-                [([&line, &script](auto b, auto e) {
-                    fostlib::push_back(script, line, fostlib::string(b, e));
-                })]
-            >> *boost::spirit::chlit<wchar_t>(L' ')
-            >> *(
-                (
-                    json_p
-                        [([&line, &script](auto j) {
-                            fostlib::push_back(script, line, j);
-                        })]
-                    | (string_p
-                        [([&line, &script](auto b, auto e) {
-                            fostlib::push_back(script, line, fostlib::string(b, e));
-                        })] >> *space_p)
-                )
-            ))
-            [([&line, &script](auto, auto) {
-                ++line;
+        | sexpr_p
+            [([&script](auto j) {
+                fostlib::push_back(script, j);
             })]
         ));
     if ( not result.full ) {
