2ba07e9405c9d1f1736106c45e09b2df58dda39c
Kirit Sælensminde <k@kirit.com>
2017-02-17 16:30:24 +0700

    Save the file content.

--- Cpp/fostgres/file.cpp
+++ Cpp/fostgres/file.cpp
@@ -20,8 +20,18 @@ fostlib::nullable<fostlib::json> fostgres::file_upload(
         auto data = fostlib::coerce<std::vector<unsigned char>>(value);
         fostlib::digester hasher{fostlib::sha256};
         hasher << data;
-        auto filename = fostlib::coerce<fostlib::hex_string>(hasher.digest()).underlying().underlying();
-        return fostlib::json(filename.c_str());
+        auto pathname = fostlib::coerce<fostlib::hex_string>(hasher.digest()).underlying().underlying();
+        const boost::filesystem::path directory = pathname.substr(0, 3);
+        const boost::filesystem::path filename = pathname.substr(3);
+        auto store_config = fostlib::setting<fostlib::json>::value(
+            "File storage", fostlib::coerce<fostlib::string>(defn["store"]));
+        auto location = fostlib::coerce<boost::filesystem::path>(store_config["path"]);
+        location /= directory;
+        boost::filesystem::create_directories(location);
+        location /= filename;
+        boost::filesystem::ofstream save(location);
+        save.write(reinterpret_cast<const char *>(data.data()), data.size());
+        return fostlib::json((directory / filename).c_str());
     } else {
         throw fostlib::exceptions::not_implemented(__func__,
             "File upload where `source` is specified");
