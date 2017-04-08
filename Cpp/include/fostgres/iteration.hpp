fdaaafcc7a24a620fbee253c9b41b5f09fe743cf
Kirit Sælensminde <k@kirit.com>
2017-04-08 12:15:37 +0700

    Add a `json_recordset` adapter

--- Cpp/include/fostgres/iteration.hpp
+++ Cpp/include/fostgres/iteration.hpp
@@ -19,5 +19,51 @@ namespace fostgres {
         column_names(fostlib::pg::recordset &&rs);
 
 
+    /// A range that allows the recordset to be iterated to produce JSON
+    /// objects, one per row.
+    class json_recordset {
+        std::vector<fostlib::string> names;
+        fostlib::pg::recordset rs;
+    public:
+        json_recordset(std::pair<std::vector<fostlib::string>, fostlib::pg::recordset> rs);
+
+        class const_iterator :
+            public std::iterator<std::input_iterator_tag, fostlib::json::object_t>
+        {
+            friend class json_recordset;
+
+            const std::vector<fostlib::string> *names;
+            fostlib::pg::recordset::const_iterator pos;
+            fostlib::json::object_t object;
+
+            const_iterator(
+                const std::vector<fostlib::string> &names,
+                fostlib::pg::recordset::const_iterator p);
+        public:
+            const_iterator()
+            : names(nullptr) {
+            }
+            const_iterator(const const_iterator &) = default;
+
+            const_iterator &operator = (const const_iterator &) = default;
+
+            bool operator == (const const_iterator &other) const {
+                return names == other.names && pos == other.pos;
+            }
+            bool operator != (const const_iterator &other) const {
+                return not (*this == other);
+            }
+
+            const_iterator &operator ++ ();
+            const_iterator operator ++ (int);
+
+            fostlib::json::object_t &operator * ();
+        };
+
+        const_iterator begin() const;
+        const_iterator end() const;
+    };
+
+
 }
 
