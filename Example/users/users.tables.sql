f658f29eb9b67fa563b4267ac58f76db87a2b470
Kirit Sælensminde <k@kirit.com>
2017-06-03 10:50:58 +0700

    Sketch out the requirements forthe multi PUT
    These are the tests needed to fix #2

--- Example/users/users.tables.sql
+++ Example/users/users.tables.sql
@@ -6,7 +6,8 @@ CREATE TABLE users (
 
 
 CREATE TABLE passwords (
-    username text NOT NULL REFERENCES users ON DELETE CASCADE INITIALLY DEFERRED,
+    username text NOT NULL REFERENCES users
+        ON DELETE CASCADE INITIALLY DEFERRED,
     hashed text NOT NULL,
     salt text NOT NULL,
     CONSTRAINT passwords_pk PRIMARY KEY(username)
@@ -14,8 +15,29 @@ CREATE TABLE passwords (
 
 
 CREATE TABLE avatar_upload (
-    username text NOT NULL REFERENCES users ON DELETE CASCADE INITIALLY DEFERRED,
+    username text NOT NULL REFERENCES users
+        ON DELETE CASCADE INITIALLY DEFERRED,
     avatar text NOT NULL,
     CONSTRAINT avatar_upload_pk PRIMARY KEY (username)
+);
+
+
+CREATE TABLE groups (
+    name text NOT NULL,
+    CONSTRAINT groups_pk PRIMARY KEY (name)
+);
+
+
+CREATE TABLE permissions (
+    name text NOT NULL,
+    CONSTRAINT permissions_pk PRIMARY KEY (name)
+);
+
+
+CREATE TABLE group_permissions (
+    group_name text NOT NULL,
+    permission_name text NOT NULL,
+    CONSTRAINT group_permissions_pk
+        PRIMARY KEY (group_name, permission_name)
 );
 
