129a51baa4c77992387c1830f6e2f1d160e1acde
Kirit Sælensminde <k@kirit.com>
2019-01-10 12:23:58 +0700

    Add correct namespace.

--- Cpp/fostgres-fg/fg.parser.cpp
+++ Cpp/fostgres-fg/fg.parser.cpp
@@ -1,8 +1,8 @@
-/*
-    Copyright 2016-2017 Felspar Co Ltd. http://support.felspar.com/
+/**
+    Copyright 2016-2019 Felspar Co Ltd. <http://support.felspar.com/>
+
     Distributed under the Boost Software License, Version 1.0.
-    See accompanying file LICENSE_1_0.txt or copy at
-        http://www.boost.org/LICENSE_1_0.txt
+    See <http://www.boost.org/LICENSE_1_0.txt>
 */
 
 
@@ -16,7 +16,7 @@
 fostlib::json fg::parse(const boost::filesystem::path &filename) {
     fostlib::string code(fostlib::utf::load_file(filename));
     std::vector<fostlib::json> script;
-    auto pos = f5::make_u32u16_iterator(code.begin(), code.end());
+    auto pos = f5::cord::make_u32u16_iterator(code.begin(), code.end());
     fg_parser<decltype(pos.first)> fg_p;
     if (boost::spirit::qi::parse(pos.first, pos.second, fg_p, script)
         && pos.first == pos.second) {
