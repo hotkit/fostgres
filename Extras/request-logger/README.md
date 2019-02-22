77b211c85b7bcca72994220796981a926cb9866d
Kirit Sælensminde <kirit@kirit.com>
2019-02-22 14:21:13 +0700

    Initial outline of the new middleware.

---
+++ Extras/request-logger/README.md
@@ -0,0 +1,10 @@
+# Request logging
+
+There are two parts:
+
+1. A view that keeps a copy of all of the log message for its inner views and saves them to the database.
+2. A set of views that allow the logs to be fetched from the database.
+
+
+## Configuration
+
---
+++ Extras/request-logger/logger.cpp
@@ -0,0 +1,49 @@
+/**
+    Copyright 2019, Proteus Technologies Co Ltd. <https://support.felspar.com/>
+
+    Distributed under the Boost Software License, Version 1.0.
+    See <http://www.boost.org/LICENSE_1_0.txt>
+*/
+
+
+#include <fost/log>
+#include <fost/urlhandler>
+
+
+namespace {
+
+
+    class capture_copy {
+        bool pass_on;
+
+      public:
+        using result_type = std::vector<fostlib::json> const &;
+        std::vector<fostlib::json> messages;
+
+        capture_copy(bool pass_on = true) : pass_on{pass_on} {}
+
+        bool operator()(const fostlib::log::message &m) {
+            messages.push_back(fostlib::coerce<fostlib::json>(m));
+            return pass_on;
+        }
+
+        result_type operator()() const { return messages; }
+    };
+
+
+    const class request_logger : public fostlib::urlhandler::view {
+      public:
+        request_logger() : view("fostgres.request-logging") {}
+
+        std::pair<boost::shared_ptr<fostlib::mime>, int> operator()(
+                const fostlib::json &config,
+                const fostlib::string &path,
+                fostlib::http::server::request &req,
+                const fostlib::host &host) const {
+            fostlib::log::scoped_sink<capture_copy> logs;
+            throw fostlib::exceptions::not_implemented(__PRETTY_FUNCTION__);
+        }
+    } c_request_logger;
+
+
+}
