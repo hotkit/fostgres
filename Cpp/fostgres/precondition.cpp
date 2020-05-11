94b118caa80213114e7a41ffa1b3934d7aca33a3
Kirit Sælensminde <kirit@kirit.com>
2020-05-11 11:24:10 +0700

    Re-order code to make alphabetical

--- Cpp/fostgres/precondition.cpp
+++ Cpp/fostgres/precondition.cpp
@@ -25,20 +25,6 @@ namespace {
             return fostlib::json{};
         }
     }
-
-    fostlib::json
-            match(const std::vector<fostlib::string> &args,
-                  fsigma::frame &stack,
-                  fostlib::json::const_iterator pos,
-                  fostlib::json::const_iterator end) {
-        auto const arg_idx =
-                stack.resolve_int(stack.argument("arg_idx", pos, end));
-        if (arg_idx > 0 && arg_idx <= args.size()) {
-            return fostlib::json{args[arg_idx - 1]};
-        }
-        return fostlib::json{};
-    }
-
     fostlib::json
             eq(fsigma::frame &stack,
                fostlib::json::const_iterator pos,
@@ -52,8 +38,6 @@ namespace {
         }
         return fostlib::json{val};
     }
-
-
     fostlib::json logic_or(
             fsigma::frame &stack,
             fostlib::json::const_iterator pos,
@@ -63,6 +47,18 @@ namespace {
                 not ev.isnull()) {
                 return ev;
             }
+        }
+        return fostlib::json{};
+    }
+    fostlib::json
+            match(const std::vector<fostlib::string> &args,
+                  fsigma::frame &stack,
+                  fostlib::json::const_iterator pos,
+                  fostlib::json::const_iterator end) {
+        auto const arg_idx =
+                stack.resolve_int(stack.argument("arg_idx", pos, end));
+        if (arg_idx > 0 && arg_idx <= args.size()) {
+            return fostlib::json{args[arg_idx - 1]};
         }
         return fostlib::json{};
     }
@@ -76,24 +72,20 @@ fsigma::frame fostgres::preconditions(
         const std::vector<fostlib::string> &args) {
     fsigma::frame f{nullptr};
 
+    f.native["eq"] = [](fsigma::frame &stack, fostlib::json::const_iterator pos,
+                        fostlib::json::const_iterator end) {
+        return eq(stack, pos, end);
+    };
     f.native["header"] = [&req](fsigma::frame &stack,
                                 fostlib::json::const_iterator pos,
                                 fostlib::json::const_iterator end) {
         return header(req, stack, pos, end);
     };
-
     f.native["match"] = [&args](fsigma::frame &stack,
                                 fostlib::json::const_iterator pos,
                                 fostlib::json::const_iterator end) {
         return match(args, stack, pos, end);
     };
-
-    f.native["eq"] = [](fsigma::frame &stack, fostlib::json::const_iterator pos,
-                        fostlib::json::const_iterator end) {
-        return eq(stack, pos, end);
-    };
-
-
     f.native["or"] = [](fsigma::frame &stack, fostlib::json::const_iterator pos,
                         fostlib::json::const_iterator end) {
         return logic_or(stack, pos, end);
