31189747175763c177f71e9932d37ceccdefd29b
SukitOwl <sukit.s@proteus-tech.com>
2020-06-30 14:00:28 +0700

    test expect in fg

---
+++ test/test-views/tests.hpp
@@ -0,0 +1,23 @@
+/**
+    Copyright 2020 Red Anchor Trading Co. Ltd.
+
+    Distributed under the Boost Software License, Version 1.0.
+    See <http://www.boost.org/LICENSE_1_0.txt>
+ */
+
+
+#pragma once
+
+
+#include <fost/urlhandler>
+
+
+namespace test {
+
+
+    namespace view {
+
+
+        extern const fostlib::urlhandler::view &chain_request;
+    }
+}
